214a7603e4663c5dc9c8e5f5d069404d42263a95
Frank Liu <gniquil@gmail.com>
2015-04-07 03:13:49 -0700

    added license and doc

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule ExMinimatch.Mixfile do
     [app: :ex_minimatch,
      version: "0.0.1",
      elixir: "~> 1.0",
+     description: description,
+     package: package,
      deps: deps]
   end
 
@@ -12,7 +14,7 @@ defmodule ExMinimatch.Mixfile do
   #
   # Type `mix help compile.app` for more information
   def application do
-    [applications: [:logger]]
+    []
   end
 
   # Dependencies can be Hex packages:
@@ -26,7 +28,26 @@ defmodule ExMinimatch.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:ex_brace_expansion, "~> 0.0.1"}
+      {:ex_brace_expansion, "~> 0.0.1"},
+      {:ex_doc, "~> 0.7", only: :dev},
+      {:markdown, github: "devinus/markdown"}
+    ]
+  end
+
+  defp description do
+    """
+    Globbing paths without walking the tree! Elixir and Erlang provide `wildcard`
+    functions in the stdlib. But these will walk the directory tree. If you simply
+    want to test whether a file path matches a glob, ExMinimatch is for you.
+    """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      contributors: ["Frank Liu"],
+      links: %{"GitHub" => "https://github.com/gniquil/ex_minimatch"},
+      licenses: ["MIT"],
     ]
   end
 end
